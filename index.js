0488e36b5cc67b9793b6bbe71724a6b88073c7ed
@SanPanda <deivis9010@gmail.com>
2025-04-12 13:31:15 -0300

    se modifica index.js para agregar el servidor express y publicar endpoint inicial

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Mi primer proyecto, Node.js!");
+//Cargando el modulo de express
+const express = require('express');
+//Instancia de express
+const app = express(); 
+ //Puerto de la aplicacion establecido en el entorno o 3000 por defecto
+ const port = process.env.PORT || 3000;
+
+ 
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {    
+    res.send('API Tecnologias para el desarrollo web is running!');
+})
+
